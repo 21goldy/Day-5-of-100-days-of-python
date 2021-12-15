61bbb6de42a2368c6930584ce7a4f66d4d80fa98
Goldy Gour <64465355+21goldy@users.noreply.github.com>
2021-12-15 09:44:35 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!\n")
+nr_letters= int(input("How many letters would you like in your password? ")) 
+nr_symbols = int(input("\nHow many symbols would you like? "))
+nr_numbers = int(input("\nHow many numbers would you like? "))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+pass_list = []
+for char in range(1, nr_letters+1):
+  pass_list += random.choice(letters)
+    
+for symbol in range(1, nr_symbols+1):
+  pass_list += random.choice(symbols)
+  
+for num in range(1, nr_numbers+1):
+  pass_list += random.choice(numbers)
+ 
+random.shuffle(pass_list)
+final = "".join(pass_list)
+print(f"\nYour auto-generated password is: {final}")
+
+  
